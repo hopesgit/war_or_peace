d4054235528e15a5d7e4bf45828967f8757a0af5
Hope Gochnour <hope.gochnour@gmail.com>
2020-08-19 17:03:31 -0600

    Add test for entire :mutually_assured_destruction Turn type trajectory

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -198,9 +198,10 @@ class TurnTest < Minitest::Test
 
     turn.award_spoils
 
-    assert_equal 2, turn.player2.deck.cards.count
     assert_equal 0, turn.player1.deck.cards.count
+    assert_equal 2, turn.player2.deck.cards.count
     assert turn.player1.has_lost?
+    refute turn.player2.has_lost?
   end
 
   def test_war_path
@@ -226,8 +227,38 @@ class TurnTest < Minitest::Test
 
     turn.award_spoils
 
-    assert_equal 6, turn.player2.deck.cards.count
     assert_equal 0, turn.player1.deck.cards.count
+    assert_equal 6, turn.player2.deck.cards.count
     assert turn.player1.has_lost?
+    refute turn.player2.has_lost?
+  end
+
+  def test_mad_path
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '8', 8)
+    deck1 = Deck.new([card1, card2, card5])
+    deck2 = Deck.new([card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal :mutually_assured_destruction, turn.type
+    assert_equal "No Winner", turn.winner
+
+    turn.pile_cards
+
+    assert_equal 0, turn.player1.deck.cards.count
+    assert_equal 0, turn.player2.deck.cards.count
+
+    turn.award_spoils
+
+    assert_equal 0, turn.player1.deck.cards.count
+    assert_equal 0, turn.player2.deck.cards.count
+    assert turn.player1.has_lost?
+    assert turn.player2.has_lost?
   end
 end
