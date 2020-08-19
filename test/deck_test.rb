ffddf7f9705d2ebdd1df473575f4dec3345e87ac
Hope Gochnour <hope.gochnour@gmail.com>
2020-08-19 12:08:16 -0600

    Add (Deck): generate cards, remove cards, determine how many and % high

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,36 +5,72 @@ require "./lib/deck"
 class DeckTest < Minitest::Test
 
   def test_it_is_a_deck_of_cards
-    deck1 = Deck.new
+    deck = Deck.new
 
-    assert_instance_of Deck, deck1
+    assert_instance_of Deck, deck
   end
 
   def test_it_can_hold_cards
-    deck1 = Deck.new
-    heartjack = Card.new(:heart, "Jack", 11)
-    heartqueen = Card.new(:heart, "Queen", 12)
-    heartking = Card.new(:heart, "King", 13)
-    cards = [heartqueen, heartking]
+    card1 = Card.new(:diamond, "Queen", 12)
+    card2 = Card.new(:spade, "3", 3)
+    card3 = Card.new(:heart, "Ace", 14)
+    cards_to_add = [card1, card2, card3]
+    deck = Deck.new(cards_to_add)
 
-    assert_equal [], deck1.cards
-
-    deck1.add_card(heartjack)
-    assert_equal [heartjack], deck1.cards
-
-    deck1.add_card(cards)
-    assert_equal [heartjack, heartqueen, heartking], deck1.cards
+    assert_equal [card1, card2, card3], deck.cards
   end
 
   def test_it_can_find_rank
-    deck1 = Deck.new
-    heartjack = Card.new(:heart, "Jack", 11)
-    heartqueen = Card.new(:heart, "Queen", 12)
-    heartking = Card.new(:heart, "King", 13)
-    cards = [heartjack, heartqueen, heartking]
-    deck1.add_card(cards)
+    card1 = Card.new(:diamond, "Queen", 12)
+    card2 = Card.new(:spade, "3", 3)
+    card3 = Card.new(:heart, "Ace", 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
 
-    assert_equal 11, deck1.rank_of_card_at(0)
-    assert_equal 13, deck1.rank_of_card_at(2)
+    assert_equal 12, deck.rank_of_card_at(0)
+    assert_equal 14, deck.rank_of_card_at(2)
+  end
+
+  def test_it_can_find_high_cards
+    card1 = Card.new(:diamond, "Queen", 12)
+    card2 = Card.new(:spade, "3", 3)
+    card3 = Card.new(:heart, "Ace", 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal [card1, card3], deck.high_ranking_cards
+  end
+
+  def test_it_can_get_percent_high_cards
+    card1 = Card.new(:diamond, "Queen", 12)
+    card2 = Card.new(:spade, "3", 3)
+    card3 = Card.new(:heart, "Ace", 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal 66.67, deck.percent_high_ranking
+  end
+
+  def test_it_can_get_rid_of_cards
+    card1 = Card.new(:diamond, "Queen", 12)
+    card2 = Card.new(:spade, "3", 3)
+    card3 = Card.new(:heart, "Ace", 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal [card1, card2, card3], deck.cards
+
+    deck.remove_card
+
+    assert_equal [card2, card3], deck.cards
+    assert_equal [card3], deck.high_ranking_cards
+    assert_equal 50.0, deck.percent_high_ranking
+  end
+
+  def test_it_can_create_cards
+    deck = Deck.new
+    deck.generate_cards
+
+    assert_equal 52, deck.cards.count
   end
 end
