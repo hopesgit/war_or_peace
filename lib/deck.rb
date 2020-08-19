009fd9c8a850b5a01bb221a0accea00f0db3d9db
Hope Gochnour <hope.gochnour@gmail.com>
2020-08-19 08:46:59 -0600

    Added ability to add card(s) to deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,4 +6,12 @@ class Deck
   def initialize
     @cards = []
   end
+
+  def generate_cards
+  end
+
+  def add_card(card_to_add)
+    @cards << card_to_add
+    @cards.flatten!
+  end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -9,4 +9,20 @@ class DeckTest < Minitest::Test
 
     assert_instance_of Deck, deck1
   end
+
+  def test_it_can_hold_cards
+    deck1 = Deck.new
+    heartjack = Card.new(:heart, "Jack", 11)
+    heartqueen = Card.new(:heart, "Queen", 12)
+    heartking = Card.new(:heart, "King", 13)
+    cards = [heartqueen, heartking]
+
+    assert_equal [], deck1.cards
+
+    deck1.add_card(heartjack)
+    assert_equal [heartjack], deck1.cards
+
+    deck1.add_card(cards)
+    assert_equal [heartjack, heartqueen, heartking], deck1.cards
+  end
 end
