39d3083431fd0fd740c048d5f7c240efb358aa2b
Hope Gochnour <hope.gochnour@gmail.com>
2020-08-19 14:21:10 -0600

    Add Player class

---
+++ lib/player.rb
@@ -0,0 +1,10 @@
+require "./lib/deck"
+require "./lib/card"
+
+class Player
+
+  def initialize(name, deck)
+    @name = name
+    @deck = deck
+  end
+end
---
+++ test/player_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require "./lib/player"
+
+class PlayerTest < Minitest::Test
+  def test_it_is_a_player
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+
+    assert_instance_of Player, player
+  end
+end
